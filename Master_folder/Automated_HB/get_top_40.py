64171fd51c77d64df39170fba451e3623fc1a6e3
raghvendra44 <68062751+raghvendra44@users.noreply.github.com>
2023-09-13 18:31:08 +0530

    Update get_top_40.py

--- Master_folder/Automated_HB/get_top_40.py
+++ Master_folder/Automated_HB/get_top_40.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-def get_files(reference_file,folder_with_ligands,parent_dir,ligplot_processing_path,n):
+def get_files(parent_dir,reference_file,folder_with_ligands,ligplot_processing_path,n):
 
     print("\n Getting short listed files - using File name...\n")
 
@@ -9,7 +9,7 @@ def get_files(reference_file,folder_with_ligands,parent_dir,ligplot_processing_p
         print("\t- Created a Folder for adding all the short listed files!")
         os.mkdir(ligplot_processing_path)
 
-    with open(parent_dir+fr"\\{reference_file}", "r", encoding='utf-8') as file:
+    with open(parent_dir + reference_file, "r", encoding='utf-8') as file:
         data = file.readlines()
         print("\t- Fetching short listed files...!")
 
@@ -20,28 +20,30 @@ def get_files(reference_file,folder_with_ligands,parent_dir,ligplot_processing_p
                 k = i[:len(i)-1]
 
             not_found = False
-            directories = os.listdir(parent_dir+fr"\\{folder_with_ligands}")
-            for j in directories:
+            for j in os.listdir(parent_dir + folder_with_ligands):
                 if(n == 1):
                     if(k in j):
-                        shutil.copy(parent_dir+fr"\\{folder_with_ligands}\\{j}",ligplot_processing_path+fr"\\{i[:len(i)-1]}.pdbqt")     # isolates the required file to another folder
+                        if("\n" in i):
+                            i = i[:-1]
+                        shutil.move(parent_dir + folder_with_ligands + j,ligplot_processing_path + i +".pdbqt")     # isolates the required file to another folder
 
                 else:
                     if(n == 2):
-                        ending = f"\\{j}"
+                        ending = j
                     elif(n == 3):
-                        ending = f"\\{j}\\out.pdbqt"
+                        ending = {j} + "\\out.pdbqt"
 
-                    with open(parent_dir+f"\\{folder_with_ligands+ending}", "r", encoding='utf-8') as file2:
+                    with open(parent_dir + folder_with_ligands + ending, "r", encoding='utf-8') as file2:
                         id = file2.read()
                         if(i in id):
-                            shutil.copy(parent_dir+fr"\\{folder_with_ligands+ending}",ligplot_processing_path+fr"\\{i[:len(i)-1]}.pdbqt")     # isolates the required file to another folder
+                            if("\n" in i):
+                                i = i[:-1]
+                            shutil.move(parent_dir+ folder_with_ligands + ending,ligplot_processing_path + i + ".pdbqt")     # isolates the required file to another folder
                 not_found = True
 
             if(not_found == False):
                 print(i," NOT FOUND!!")
     file.close()
-
 
     pdbqt = sum(f.endswith('.pdbqt') for f in os.listdir(ligplot_processing_path))
     print("\t- SUCCESS: Fetched all the",pdbqt,"short listed files!!")
