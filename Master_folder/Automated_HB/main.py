7aa9d9fe9aabac11978928596daefbffd92d38e7
raghvendra44 <68062751+raghvendra44@users.noreply.github.com>
2023-09-13 23:17:06 +0530

    Update main.py
    Paths updated

--- Master_folder/Automated_HB/main.py
+++ Master_folder/Automated_HB/main.py
@@ -1,30 +1,37 @@
 # Importing pakages
 import streamlit as st
 import sys
-sys.path.append(r'path\to\Automated_HB')
+import os
+from pathlib import Path
+path = Path(__file__).parent
+#print("Current Directory : ",path)
+sys.path.append(path)
+
+#Important File paths
+parent_dir= str(path)[:-len("AUTOMATED_HB")] # Path to "Master_folder"
+ligplot_processing_path = parent_dir+"working_files\\" # folder in which all the files for which the data is to be generated is placed inside it.
+
+if(os.getcwd() != path):
+    os.chdir(path)
 
 from documentation import documentation
 from home import home
 
-#Important File paths
-parent_dir= "path\to\Master_folder" + "\\"
-ligplot_processing_path = parent_dir+"\\top_40_files"
-
 def main():
-    global parent_dir,ref_file_for_splitting,reference_file,folder_with_ligands,ligplot_processing_path
+    global parent_dir,ligplot_processing_path
     menu = ["Home","Docs","About Us"]
     choice = st.sidebar.selectbox("Functions", menu)
 
     if choice == "Home":
         st.title("VinaLigGen")
-        st.header("Get Hydrogen Bonds and Hydrophobic Interactions")
+        st.header("Generate bulk Hydrogen Bonds and Hydrophobic Interactions")
         home(parent_dir,ligplot_processing_path)
 
     if choice == "Docs":
         st.title("Documentation")
         documentation(parent_dir)
 
-    elif choice == "About Us":
+    if choice == "About Us":
         st.subheader("About Us")
         st.text("""This Server is made by Students of PES UNIVERSITY under
 Guidance of Dr. Prashantha Karunakar.
