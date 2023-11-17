33cc405b90639b1712f83d0a48ca718d403ff04f
raghvendra44 <68062751+raghvendra44@users.noreply.github.com>
2023-11-17 22:55:08 +0530

    Optimized shortlist_files.py

--- Master_folder/Automated_HB/shortlist_files.py
+++ Master_folder/Automated_HB/shortlist_files.py
@@ -2,56 +2,66 @@ import os
 import shutil
 
 def get_files(parent_dir,reference_file,folder_with_ligands,ligplot_processing_path,n):
-
     print("\n Getting short listed files - using File name...\n")
 
     if(os.path.exists(ligplot_processing_path) != True):
         print("\t- Created a Folder for adding all the short listed files!")
         os.mkdir(ligplot_processing_path)
+
     if(os.path.exists(parent_dir + reference_file)):
+        files_in_ligands_folder = os.listdir(parent_dir + folder_with_ligands)
+        count_ids = 0
 
         with open(parent_dir + reference_file, "r", encoding='utf-8') as file:
-            data = file.readlines()
+            data = [i.strip() for i in file.readlines() if i.strip()]
+            count_ids = sum(1 for line in data if line)
+
             print("\t- Fetching short listed files...!")
 
             for i in data:
                 try:
-                    k = i.split("_")[1]     # ZINC ID
+                    if("\n" != i):
+                        file_id = i.split("_")[1] if "uff" != i.split("_")[1] else i.split("_")[0] # Molecule ID Extraction
                 except IndexError:
-                    k = i[:len(i)-1]
+                    if("\n" !=i):
+                        file_id = i.strip()
 
-                not_found = False
-                for j in os.listdir(parent_dir + folder_with_ligands):
-                    if(n == 1):
-                        if(k in j):
-                            if("\n" in i):
-                                i = i[:-1]
-                            shutil.move(parent_dir + folder_with_ligands + j,ligplot_processing_path + i +".pdbqt")     # isolates the required file to another folder
+                not_found = True
+                ending = ''
+                for ligand in files_in_ligands_folder:
+                    if( n == 1 and (file_id in ligand or file_id[:-1] in ligand)):
+                        move_file(ligand, i, parent_dir + folder_with_ligands, ligplot_processing_path)
+                        files_in_ligands_folder.remove(ligand)
+                        not_found = False
+                        break
 
-                    else:
-                        if(n == 2):
-                            ending = j
-                        elif(n == 3):
-                            ending = {j} + "\\out.pdbqt"
+                    elif n == 2:
+                        ending = ligand
+                    elif n == 3:
+                        ending = ligand + "\\out.pdbqt"
 
+                    if n > 1:
                         with open(parent_dir + folder_with_ligands + ending, "r", encoding='utf-8') as file2:
-                            id = file2.read()
-                            if(i in id):
-                                if("\n" in i):
-                                    i = i[:-1]
-                                shutil.move(parent_dir+ folder_with_ligands + ending,ligplot_processing_path + i + ".pdbqt")     # isolates the required file to another folder
-                    not_found = True
+                            if i in file2.read():
+                                shutil.move(parent_dir+ folder_with_ligands + ending,ligplot_processing_path + ending)     # isolates the required file to another folder
+                                files_in_ligands_folder.remove(ligand)
+                                not_found = False
+                                break
 
-                if(not_found == False):
-                    print(i," NOT FOUND!!")
+                if(not_found):
+                    print(f"\t- ERROR: {[file_id]} NOT FOUND!! Please Re-check the ID (Note: the ID is intentionally printed as a list)")
         file.close()
         pdbqt = sum(f.endswith('.pdbqt') for f in os.listdir(ligplot_processing_path))
-        print("\t- SUCCESS: Fetched all the",pdbqt,"shortlisted files!!")
+        print(f"\t- SUCCESS: Fetched {pdbqt}/{count_ids} shortlisted files!!")
 
     else:
-        if not (os.path.exists(ligplot_processing_path)): # Checks if "working_files" Folder exists, if not creates one
-            print("\t- Created a Folder for adding all the short listed files!")
-            os.mkdir(ligplot_processing_path)
+        print("\t- Created a Folder for adding all the short listed files!")
+        os.mkdir(ligplot_processing_path)
 
         for i in os.listdir(parent_dir + folder_with_ligands):
-            shutil.move(parent_dir + folder_with_ligands + i, ligplot_processing_path + i) # Moves all the files that are to be processed to "working_files" Folder
+            move_file(i, "", parent_dir + folder_with_ligands, ligplot_processing_path) # Moves all the files that are to be processed to "working_files" Folder
+
+def move_file(filename, shortlisted_id, source_path, destination_path):
+    if "\n" in shortlisted_id:
+        shortlisted_id = shortlisted_id[:-1]
+    shutil.move(source_path + filename, destination_path + filename)
